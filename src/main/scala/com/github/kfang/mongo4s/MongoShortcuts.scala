f44de5628edd62f7edbf777805797fa0aeb41b2a
hkr.fang <hkr.fang@gmail.com>
2015-03-24 17:44:20 -0700

    more mongo shortcuts

--- src/main/scala/com/github/kfang/mongo4s/MongoShortcuts.scala
+++ src/main/scala/com/github/kfang/mongo4s/MongoShortcuts.scala
@@ -4,6 +4,10 @@ import reactivemongo.bson._
 
 import scala.util.Try
 
+/**
+ * Obviously this is not going to be exhaustive.  If you want a shortcut added,
+ * either file and issue or submit a pull request.  The community will thank you.
+ */
 trait MongoShortcuts {
 
   //yeah.. checkstyle doesn't like but w/e, its convenient
@@ -15,6 +19,11 @@ trait MongoShortcuts {
 
   def $in(seq: Seq[BSONValue]): (String, BSONValue) = "$in" -> BSONArray(seq)
   def $nin(seq: Seq[BSONValue]): (String, BSONValue) = "$nin" -> BSONArray(seq)
+  def $exists(b: Boolean): (String, BSONValue) = "$exists" -> BSONBoolean(value = b)
+
+  /**********************************************************************************
+   * Field Operators
+   *********************************************************************************/
 
   def $inc(fieldAmount: (String, Double), more: (String, Double)*): (String, BSONValue) =
     "$inc" -> Bdoc(more.:+(fieldAmount).toMap.mapValues(BSONDouble.apply))
@@ -45,6 +54,29 @@ trait MongoShortcuts {
     case false => "$currentDate" -> Bdoc(field -> Bdoc("$type" -> "timestamp"))
   }
 
+  /**********************************************************************************
+    * Array Operators
+    *********************************************************************************/
+
+  def $addToSet(fieldVal: Producer[(String, BSONValue)], more: Producer[(String, BSONValue)]*): (String, BSONValue) =
+    "$addToSet" -> Bdoc(more.:+(fieldVal): _*)
+
+  def $pop(field: String, popFirst: Boolean = true): (String, BSONValue) = popFirst match {
+    case true  => "$pop" -> Bdoc(field -> -1)
+    case false => "$pop" -> Bdoc(field -> 1)
+  }
+
+  def $pull(field: String, criteria: BSONValue): (String, BSONValue) =
+    "$pull" -> Bdoc(field -> criteria)
+
+  def $pull[T](field: String, criteria: T)(implicit writer: BSONWriter[T, BSONValue]): (String, BSONValue) =
+    "$pull" -> Bdoc(field -> writer.write(criteria))
+
+  def $push(field: String, value: BSONValue): (String, BSONValue) =
+    "$push" -> Bdoc(field -> value)
+
+  def $push[T](field: String, value: T)(implicit writer: BSONDocumentWriter[T]): (String, BSONValue) =
+    "$push" -> Bdoc(field -> writer.write(value))
 }
 
 object MongoShortcuts extends MongoShortcuts
